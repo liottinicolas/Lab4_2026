37b696aaa35f81a1684648b61bcb64ea119bd3be
liottinicolas <nicolasliotti92@gmail.com>
2026-06-01 23:28:35 -0300

    Agregue fábrica y puse SINGLETON a las controladoras (es mas facil)

--- include/ControladorGestionViajes.h
+++ include/ControladorGestionViajes.h
@@ -1,23 +1,27 @@
 #ifndef CONTROLADORGESTIONVIAJES_H
 #define CONTROLADORGESTIONVIAJES_H
 
-#include "IControladorGestionViajes.h"
 #include "DTListarViaje.h"
+#include "IControladorGestionViajes.h"
+#include "ManejadorViaje.h"
+#include "ManejadorUsuario.h"
 #include <string>
 #include <vector>
-#include "ManejadorViaje.h"
+
 
 class ControladorGestionViajes : public IControladorGestionViajes {
 private:
-    static ControladorGestionViajes* instancia;
-    
-    ControladorGestionViajes();
+  static ControladorGestionViajes *instancia;
+  ControladorGestionViajes();
+
+  ManejadorUsuario *mu;
+  ManejadorViaje *mv;
 
 public:
-    static ControladorGestionViajes* getInstancia(); 
+  static ControladorGestionViajes *getInstancia();
 
-    std::vector<DTListarViaje> listarViajes(std::string nickname) override;
-    std::vector<DTListarViaje> listarViajes() override;
+  std::vector<DTListarViaje> listarViajes(std::string nickname) override;
+  std::vector<DTListarViaje> listarViajes() override;
 };
 
 #endif
